0b3993c6e4f30d9effbcc5c8f87c3e3fd209d4f4
Vinayak <vinayaksharma111@gmail.com>
2020-10-22 19:37:47 +0530

    initial commit

---
+++ URL Check/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  URL Check
+//
+//  Created by Vinayak Sharma on 22/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
