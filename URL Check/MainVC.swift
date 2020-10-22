4ce3b0e2b9987f509540f35209a8d3084725493f
Vinayak <vinayaksharma111@gmail.com>
2020-10-22 20:05:07 +0530

    some changes

--- URL Check/MainVC.swift
+++ URL Check/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  URL Check
 //
 //  Created by Vinayak Sharma on 22/10/20.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
